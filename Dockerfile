53c957858abc013670efccbf06e5118985493b96
5untik <artem.fayda@mail.ru>
2025-05-14 18:24:32 +0400

    Базовая настройка проекта, запуск сервера

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM golang:1.24.1
 WORKDIR /app
 
 COPY go.mod ./
-COPY go.sum ./
 RUN go mod download
 
 COPY . .
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Ответ")
+	})
+
+	fmt.Println("Сервер запущен!")
+	error1 := http.ListenAndServe(":8080", nil)
+	if error1 != nil {
+		log.Fatal("Произошла ошибка при запуске сервера:", error1)
+	}
 }
